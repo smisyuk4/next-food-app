6994678d9c32d544bafcf086f1ec6175789ad78c
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-07-26 21:12:16 +0300

    write meals grid, add sqlite

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+
+import MealsGrid from '@/components/meals/meals-grid';
+import styles from './page.module.css';
+
 export default function MealsPage() {
-  return <h1>Meals page</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={styles.highlight}>by you</span>
+          <p>
+            Choose your favorite recipe and cook it yourself. It is easy and
+            fun!
+          </p>
+          <p className={styles.cta}>
+            <Link href='/meals/share'>Share your favorite recipe</Link>
+          </p>
+        </h1>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
